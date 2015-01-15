2065319c297ce5abf14bb5070a8c0fa60547ea8d
Geo Milano <george_limkt@hotmail.com>
2015-01-15 11:12:55 +0800

    15/1/2015 geo change

--- app/controllers/play.js
+++ app/controllers/play.js
@@ -26,15 +26,16 @@ function setPicker2()
 {
 	console.log("picker2");
 	var data = [];
+	var column2 = Ti.UI.createPickerColumn();
 	for(var i = 0 ; i < detailsValue.length; i++)
 	{
-		
-		data[i]=Ti.UI.createPickerRow({title:detailsValue[i].runner_id});
+		row =Ti.UI.createPickerRow({title:detailsValue[i].runner_id});
+		column2.addRow(row);
 	}
 	console.log("runner_id:"+detailsValue[0].runner_id);
 	console.log("runner_id:"+detailsValue[1].runner_id);
-	console.log(data);
-	$.picker2.add(data);
+	console.log(column2);
+	$.picker2.add([column2]);
 	$.picker2.selectionIndicator = true;
 }
 
@@ -54,8 +55,8 @@ if(Ti.Platform.osname == "android")
 if(Ti.Platform.osname == "iphone" || Ti.Platform.osname == "ipad")
 {
 	console.log("ios picker setter");
-	$.picker1.setSelectedRow(0,(infoValue.length-1),false);
-	$.picker2.setSelectedRow(0,(detailsValue.length-1),false);
+	//$.picker1.setSelectedRow(0,(infoValue.length-1),false);
+	//$.picker2.setSelectedRow(0,(detailsValue.length-1),false);
 	$.picker3.setSelectedRow(0,8,false);
 }
 
