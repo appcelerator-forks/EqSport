11e5dd58924f47ae5f367f972756be27721001d2
Geo Milano <george_limkt@hotmail.com>
2015-02-02 15:15:29 +0800

    updated with SOAP

--- app/lib/api.js
+++ app/lib/api.js
@@ -515,6 +515,47 @@ exports.popup = function(subView,config){
 	return popupWin;
 };
 
+//Today Transaction History
+exports.todayTransactionHistory = function (ex){
+	var client = Ti.Network.createHTTPClient();
+	client.onload = function() { 
+		var doc = this.responseXML.documentElement;
+		var code = doc.getElementsByTagName('soap:Code').item(0).textContent;
+		var reason = doc.getElementsByTagName('soap:Reason').item(0).textContent;
+		console.log(code);
+		console.log(reason);  
+	};
+	var soapRequest = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n" +
+	"<soap12:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" \n" +
+	"xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" \n" +
+	"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \n" +
+	"xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" \n" +
+	"xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" \n" +
+	"xmlns:wsse=\"http://schemas.xmlsoap.org/ws/2002/12/secext\"> \n" +
+	"<soap12:Body id=\"_0\"> \n" +
+	"<accCurrentDayTransactions xmlns=\"http://tempuri.org/\"> \n" +
+	"<sLogin>mitcrm</sLogin> \n" +
+	"<sPassword>m1tcrm</sPassword> \n" +
+	"<sTranid>1234567904</sTranid> \n" +
+	"<sTellerId>9999</sTellerId> \n" +
+	"<sTellerPin>9999</sTellerPin> \n" +
+	"<sAccId>60938004</sAccId> \n" +
+	"<sRto>1</sRto> \n" +
+	"<sNfo>0</sNfo> \n" +
+	"<sDeposits>0</sDeposits> \n" +
+	"<sWithdrawal>0</sWithdrawal> \n" +
+	"<sAccountAccess>0</sAccountAccess> \n" +
+	"<sAccountRelease>0</sAccountRelease> \n" +
+	"<sDXP>0</sDXP> \n" +
+	"<sCurrentDayTransactions>1</sCurrentDayTransactions> \n" +
+	"</accCurrentDayTransactions> \n" +
+	"</soap12:Body> \n" +
+	"</soap12:Envelope>";
+	client.open('POST', 'http://'+Ti.App.Properties.getString('eqUrl')+'/pmpcrm/service.asmx');//?op=accCurrentDayTransactions
+	//client.open('POST','http://175.143.5.179//pmpcrm/service.asmx');
+	client.send({xml: soapRequest});
+};
+
 //private function
 function onErrorCallback(e) {
 	var common = require('common');
